0430bbdeb268d1761fedf9c11e4098a33f69c645
Raymond Feng <Feng.R018@gmail.com>
2025-10-11 00:14:41 -0400

    attempt at part 1

--- src/renderers/naive.ts
+++ src/renderers/naive.ts
@@ -18,10 +18,18 @@ export class NaiveRenderer extends renderer.Renderer {
             label: "scene uniforms bind group layout",
             entries: [
                 // TODO-1.2: add an entry for camera uniforms at binding 0, visible to only the vertex shader, and of type "uniform"
-                { // lightSet
+                {   // camera uniforms
+                    binding: 0,
+                    visibility: GPUShaderStage.VERTEX,
+                    buffer: {
+                        type: "uniform"
+                    }
+                }, { // lightSet
                     binding: 1,
                     visibility: GPUShaderStage.FRAGMENT,
-                    buffer: { type: "read-only-storage" }
+                    buffer: { 
+                        type: "read-only-storage" 
+                    }
                 }
             ]
         });
@@ -34,6 +42,11 @@ export class NaiveRenderer extends renderer.Renderer {
                 // you can access the camera using `this.camera`
                 // if you run into TypeScript errors, you're probably trying to upload the host buffer instead
                 {
+                    binding: 0,
+                    resource: {
+                        buffer: this.camera.uniformsBuffer
+                    }
+                }, {
                     binding: 1,
                     resource: { buffer: this.lights.lightSetStorageBuffer }
                 }
@@ -106,6 +119,7 @@ export class NaiveRenderer extends renderer.Renderer {
         renderPass.setPipeline(this.pipeline);
 
         // TODO-1.2: bind `this.sceneUniformsBindGroup` to index `shaders.constants.bindGroup_scene`
+        renderPass.setBindGroup(shaders.constants.bindGroup_scene, this.sceneUniformsBindGroup);
 
         this.scene.iterate(node => {
             renderPass.setBindGroup(shaders.constants.bindGroup_model, node.modelBindGroup);
